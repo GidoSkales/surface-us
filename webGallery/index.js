a62492e80127167d59c4dedd206b380a706a8dc0
Gido Skales <66243914+GidoSkales@users.noreply.github.com>
2020-10-12 16:08:58 +0100

    Add files via upload

---
+++ webGallery/index.js
@@ -0,0 +1,8 @@
+const searchMe = document.querySelector("#searchUs");
+const search = () => {
+    searchMe.addEventListener("click", respondTo);
+}
+function respondTo() {
+    window.location = "";
+}
+const feedBack = document.querySelector(".random-photos");
