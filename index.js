82c25098b0508a0ea71381390dce076719269b68
Gido Skales <66243914+GidoSkales@users.noreply.github.com>
2020-10-26 14:19:52 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const searchMe = document.querySelector("#searchUs");
-const search = () => {
-    searchMe.addEventListener("click", respondTo);
-}
-function respondTo() {
-    window.location = "";
-}
-const feedBack = document.querySelector(".random-photos");
